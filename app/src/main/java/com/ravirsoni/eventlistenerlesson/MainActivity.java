a8e6c8fcac10c198bc448c8233551ab09854e30b
ravirsoni <ravirsoni@gmail.com>
2016-12-01 18:52:15 -0800

    # Added Logic to change Backgroun Color for main_activity

--- app/src/main/java/com/ravirsoni/eventlistenerlesson/MainActivity.java
+++ app/src/main/java/com/ravirsoni/eventlistenerlesson/MainActivity.java
@@ -1,7 +1,13 @@
 package com.ravirsoni.eventlistenerlesson;
 
+import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.RelativeLayout;
+
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +15,26 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button button = (Button) findViewById(R.id.bt_click_changecolor);
+        button.setOnClickListener(new OnClickListener(){
+
+            @Override
+            public void onClick(View view) {
+                RelativeLayout relative = (RelativeLayout) findViewById(R.id.activity_main) ;
+                    relative.setBackgroundColor(Color.GREEN);
+            }
+
+
+    });
+
+
+
     }
+
 }
+
+
+
+
+
