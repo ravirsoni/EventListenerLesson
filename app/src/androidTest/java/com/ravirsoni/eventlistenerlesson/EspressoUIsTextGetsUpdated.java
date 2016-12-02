67f8e081ada580dcbe95c20216e633f1708b4e44
ravirsoni <ravirsoni@gmail.com>
2016-12-02 14:59:21 -0800

    # Added Logic to change Backgroun Color for main_activity #Added 3 EspressoTest : 1) Correct Text is present 2) TextGets updated and shows the Correct Text

--- app/src/androidTest/java/com/ravirsoni/eventlistenerlesson/EspressoUIsTextGetsUpdated.java
+++ app/src/androidTest/java/com/ravirsoni/eventlistenerlesson/EspressoUIsTextGetsUpdated.java
@@ -1,45 +1,38 @@
 package com.ravirsoni.eventlistenerlesson;
 
 import android.support.test.rule.ActivityTestRule;
-
 import org.junit.Rule;
 import org.junit.Test;
-
-import javax.annotation.Priority;
-
 import static android.support.test.espresso.Espresso.onView;
 import static android.support.test.espresso.action.ViewActions.click;
 import static android.support.test.espresso.assertion.ViewAssertions.matches;
-import static android.support.test.espresso.matcher.ViewMatchers.isClickable;
 import static android.support.test.espresso.matcher.ViewMatchers.withId;
 import static android.support.test.espresso.matcher.ViewMatchers.withText;
 
 /**
- * Created by ravirsoni on 12/2/16.  EspressoUITextPresent
+ * Created by ravirsoni on 12/2/16. Test to Check TextGetsUpdated.
+ * Text is getting updated properly or not.
+ *
  */
 
-public class EspressoUITestTextPresent {
+public class EspressoUIsTextGetsUpdated {
 
     @Rule
     public ActivityTestRule<MainActivity> mainActivityActivityTestRule = new ActivityTestRule<>(MainActivity.class);
 
     @Test
-    public void checkTextIsPresent(){
-
-        // validate that the Correct Text is Present.
+    public void checkTextGetsUpdated (){
 
         onView(withId(R.id.tv_activity_main))
                 .check(matches(withText("Click to change Text !!!")));
+
+
+        onView(withId(R.id.bt_clicktochange_change_text))
+                .perform(click());
+
+         onView(withText(R.string.tx_updated_Text))
+                .check(matches(withText("This is Updated Text")));
+
     }
-
-//    @Test
-//    public void checkTextGetsUpdated(){
-//
-//        onView(withId(R.id.bt_clicktochange_change_text))
-//                .perform(click())
-//                .check(matches(withText("This is Updated Text")));
-//    }
-
-
 
 }
